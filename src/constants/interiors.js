4e8831429a6698c3bcfa60b019c5c72b701ebcad
James <jmkim@atomypark.com>
2025-05-26 10:38:00 +0900

    add

--- src/constants/interiors.js
+++ src/constants/interiors.js
@@ -2,11 +2,11 @@ export const interiors = [
   {
     key: 'allBlack',
     name: 'All Black',
-    img: '/colors/interiors/all-black.avif',
+    img: '/tesla-estimator/colors/interiors/all-black.avif',
   },
   {
     key: 'blackWhite',
     name: 'Black & White',
-    img: '/colors/interiors/black-white.avif',
+    img: '/tesla-estimator/colors/interiors/black-white.avif',
   },
 ];
